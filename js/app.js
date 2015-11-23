f4a3ba796aa19a7797d5b7a39c4f5261161d8b13
Shigenaka <mshig19@uw.edu>
2015-11-22 17:11:48 -0800

    ffinished everything but readme

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var myApp = angular.module('myApp', ['ui.router'])
+
+// Config route provider
+.config(function($stateProvider) {
+    $stateProvider
+  .state('home', {
+    url:'',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeController',
+  })
+  .state('projects', {
+    url:'/projects',
+    templateUrl: 'templates/projects.html',
+    controller: 'ProjectsController',
+  })
+  .state('athletics', {
+    url:'/athletics',
+    templateUrl: 'templates/athletics.html',
+    controller: 'AthleticsController',
+  })
+})
+
+// Home page controller
+.controller('HomeController', function($scope, $http){
+
+})
+
+// Projects page controller
+.controller('ProjectsController', function($scope, $http){
+  $http.get('data/projects.json').then(function(response) {
+        $scope.data = response.data;
+        console.log($scope.data);
+    });
+})
+
+// Athletics page controller
+.controller('AthleticsController', function($scope, $http){
+  $http.get('data/athletics.json').then(function(response) {
+        $scope.data = response.data;
+        console.log($scope.data);
+    });
+})
